3547e37df63078f6c5bb631b100e0909aa987f4b
Antoine Maitre <ant.maitre@gmail.com>
2018-01-16 14:22:56 +0100

    feat(dt): refactored interfaces names to be more accurate feat(dt): fixed some starting errors feat(user): - added create method in user service feat(game): - added create method in game service
    Signed-off-by: Antoine Maitre <ant.maitre@gmail.com>

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,11 @@
 /**
  * Created by tdoret on 15/01/2018.
  */
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
-import { UserSchema } from './schemas/user.schema';
+import {Module} from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
+import {UserController} from './user.controller';
+import {UserService} from './user.service';
+import {UserSchema} from './schemas/user.schema';
 import {RegisterTokenSchema} from './schemas/registerToken.schema';
 
 @Module({
